27afe0c2b4419200c40bd2b93e1d2059551afa76
Aldo <aldofieuw@hotmail.com>
2017-05-26 19:25:08 +0200

    Batman?

--- app/src/main/java/com/aldofieuw/android/musicplayer/MainActivity.java
+++ app/src/main/java/com/aldofieuw/android/musicplayer/MainActivity.java
@@ -1,13 +1,43 @@
 package com.aldofieuw.android.musicplayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    /* make media player */
+        mediaPlayer = MediaPlayer.create(this, R.raw.chopin);
+
+        /* click listeners */
+        Button playButton = (Button) findViewById(R.id.play_button);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Play", Toast.LENGTH_SHORT).show();
+                mediaPlayer.start();
+            }
+        });
+        Button pauseButton = (Button) findViewById(R.id.pause_button);
+
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Pause", Toast.LENGTH_SHORT).show();
+                mediaPlayer.pause();
+            }
+        });
     }
 }
